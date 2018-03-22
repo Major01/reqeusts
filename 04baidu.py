b6c988b3dc85535fcb577a801b26cb1bf312c578
e <1293963839@qq.com>
2018-03-22 22:08:18 +0800

    【更新】更新百度翻译接口

--- 04baidu.py
+++ 04baidu.py
@@ -2,14 +2,17 @@
 import requests
 import json
 
-post_url = ('http://fanyi.baidu.com/langdetect')
+post_url = 'https://fanyi.baidu.com/extendtrans'
 
+en = input('请输入你要翻译的内容：')
 post_data = {
-    'query':'人生苦短'
+    'query': en,
+    'from': 'zh',
+    'to': 'en'
 }
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+headers = {"User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"}
 
 r = requests.post(post_url, headers=headers, data=post_data)
 
 dict_response = json.loads(r.content.decode())
-print(dict_response)
+print('要翻译的内容是',en,'翻译为', dict_response['data']['st_tag'])
